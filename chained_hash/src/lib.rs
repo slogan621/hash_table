aa360fd51db3c43de4e8c354f5953f04350cf679
Syd Logan <slogan621@gmail.com>
2023-12-28 23:47:57 -0800

    implement chained hash delete

--- chained_hash/src/lib.rs
+++ chained_hash/src/lib.rs
@@ -35,14 +35,13 @@ impl<'a, U: std::cmp::PartialEq + std::fmt::Debug> Iterator for ChainedHashItera
     type Item = &'a Link<U>;
 
     fn next(&mut self) -> Option<Self::Item> {
-
         let current = self.next;
 
         match &*current {
             None => {
                 self.next = &None;
                 None
-            },
+            }
             Some(node) => {
                 self.next = &node.next;
                 Some(current)
@@ -53,7 +52,7 @@ impl<'a, U: std::cmp::PartialEq + std::fmt::Debug> Iterator for ChainedHashItera
 
 impl<U: std::cmp::PartialEq> List<U> {
     fn iter(&self) -> ChainedHashIterator<'_, U> {
-        ChainedHashIterator {next: &self.head }
+        ChainedHashIterator { next: &self.head }
     }
 }
 
@@ -114,77 +113,30 @@ impl<U: std::marker::Copy + std::fmt::Debug + std::cmp::PartialEq> HashTable<U> 
     }
 
     fn delete(&mut self, key: u16) -> Result<(), HashTableError> {
-        let x : usize = self.hash(key.clone()).into();
+
+        let x: usize = self.hash(key.clone()).into();
         if self.table[x] == (List { head: None }) {
             return Err(HashTableError::NotFound);
         }
 
-        let mut prev = None;
-        let mut cur = &self.table[x].head;
+        // see https://stackoverflow.com/questions/53554862/delete-a-node-in-singly-linked-list-in-rust
 
+        let mut current = &mut self.table[x].head;
         loop {
-            match cur {
-                Some(ref value) => {
-                    println!("top of loop value is {:?}", value);
-                    if value.data.key == key {
-                        if prev == None.as_ref() {
-                            // head of list
-                            println!("deleting key {:?} from head of list",
-                                key);
-                            self.table[x].head = None;
-                            //let _ = mem::replace(&self.table[x].head, Link::More(*value));
-                        } else {
-                            println!("deleting key {:?} from middle of list",
-                                key);
-                            println!("before replace prev {:?} cur {:?}",
-prev, cur);
-                            //let prev = mem::replace(&mut prev, &*cur);
-                            let _ = mem::replace(&mut prev, Some(cur));
-                            println!("after replace prev {:?} cur {:?}",
-prev, cur);
-                            println!("after replace table {:?}", self.table[x]);
-                        }
-                        return Ok(());
-                    } else {
-                        prev = Some(cur);
-                        cur = &value.next;
-                    }
-                },
-                _ => {}, // can't really happen
+            match current {
+                None => break,
+                Some(node) if node.data.key == key => {
+                    *current = node.next.take();
+                    return Ok(());
+                }
+                Some(node) => {
+                    current = &mut node.next;
+                }
             }
         }
-
-/*
-        println!("before delete table {:?}", self.table[x]);
-        //let mut cur_link = mem::replace(&mut self.table[x].head, None);
-        let &mut cur_link = &mut self.table[x].head;
-        let mut prev_link = &None;
-        
-        while let Some(ref boxed_node) = cur_link {
-            //cur_link = mem::replace(&mut boxed_node.next, None);
-            prev_link = &cur_link;
-            cur_link = boxed_node.next
-        }
-        println!("after delete table {:?}", self.table[x]);
-/*
-        for elem in self.table[x].iter() {
-            match elem {
-                Link::More(value) => {
-                    if value.data.key == key {
-                        let _ = mem::replace(&mut prev, &value.next);
-                        return Ok(());
-                    } else {
-                        prev = &value.next;
-                    }
-                },
-                _ => {}, // can't really happen
-            }
-        }
-*/
-*/
-
         Err(HashTableError::NotFound)
     }
+
     fn lookup(&self, key: u16) -> Result<U, HashTableError>
     where
         U: Copy,
@@ -201,8 +153,8 @@ prev, cur);
                     if value.data.key == key {
                         return Ok(**(value.data.data.as_ref()).unwrap());
                     }
-                },
-                _ => {},
+                }
+                _ => {}
             }
         }
 
@@ -274,97 +226,97 @@ mod tests {
         assert_eq!(ret.is_ok(), false);
     }
 
-        #[test]
-        fn can_create_basic_hash_and_delete() {
-            let mut x = ChainedHashBuilder::<u16>::new().with_capacity(3).build();
-            assert_eq!(x.get_capacity(), 3);
-            let mut item = 10;
-            assert!(x.insert(1, item).is_ok());
-            item += 10;
-            assert!(x.insert(2, item).is_ok());
-            item += 10;
-            assert!(x.insert(3, item).is_ok());
-            item += 10;
-            assert!(x.insert(4, item).is_ok());
-            item += 10;
-            assert!(x.insert(5, item).is_ok());
-            item += 10;
-            assert!(x.insert(6, item).is_ok());
+    #[test]
+    fn can_create_basic_hash_and_delete() {
+        let mut x = ChainedHashBuilder::<u16>::new().with_capacity(3).build();
+        assert_eq!(x.get_capacity(), 3);
+        let mut item = 10;
+        assert!(x.insert(1, item).is_ok());
+        item += 10;
+        assert!(x.insert(2, item).is_ok());
+        item += 10;
+        assert!(x.insert(3, item).is_ok());
+        item += 10;
+        assert!(x.insert(4, item).is_ok());
+        item += 10;
+        assert!(x.insert(5, item).is_ok());
+        item += 10;
+        assert!(x.insert(6, item).is_ok());
 
-            let ret = x.lookup(1);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 10);
-            let ret = x.lookup(2);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 20);
-            let ret = x.lookup(3);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 30);
-            let ret = x.lookup(4);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 40);
-            let ret = x.lookup(5);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 50);
-            let ret = x.lookup(6);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 60);
+        let ret = x.lookup(1);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 10);
+        let ret = x.lookup(2);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 20);
+        let ret = x.lookup(3);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 30);
+        let ret = x.lookup(4);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 40);
+        let ret = x.lookup(5);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 50);
+        let ret = x.lookup(6);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 60);
 
-            let ret = x.delete(1);
-            assert!(ret.is_ok());
-            let ret = x.lookup(1);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(2);
-            assert!(ret.is_ok());
-            let ret = x.lookup(2);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(3);
-            assert!(ret.is_ok());
-            let ret = x.lookup(3);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(4);
-            assert!(ret.is_ok());
-            let ret = x.lookup(4);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(5);
-            assert!(ret.is_ok());
-            let ret = x.lookup(5);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(6);
-            assert!(ret.is_ok());
-            let ret = x.lookup(6);
-            assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(1);
+        assert!(ret.is_ok());
+        let ret = x.lookup(1);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(2);
+        assert!(ret.is_ok());
+        let ret = x.lookup(2);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(3);
+        assert!(ret.is_ok());
+        let ret = x.lookup(3);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(4);
+        assert!(ret.is_ok());
+        let ret = x.lookup(4);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(5);
+        assert!(ret.is_ok());
+        let ret = x.lookup(5);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(6);
+        assert!(ret.is_ok());
+        let ret = x.lookup(6);
+        assert_eq!(ret.is_ok(), false);
 
-            // see if we can insert them again
+        // see if we can insert them again
 
-            let mut item = 100;
-            assert!(x.insert(1, item).is_ok());
-            item += 100;
-            assert!(x.insert(2, item).is_ok());
-            item += 100;
-            assert!(x.insert(3, item).is_ok());
+        let mut item = 100;
+        assert!(x.insert(1, item).is_ok());
+        item += 100;
+        assert!(x.insert(2, item).is_ok());
+        item += 100;
+        assert!(x.insert(3, item).is_ok());
 
-            let ret = x.lookup(1);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 100);
-            let ret = x.lookup(2);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 200);
-            let ret = x.lookup(3);
-            assert!(ret.is_ok());
-            assert_eq!(ret.unwrap(), 300);
+        let ret = x.lookup(1);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 100);
+        let ret = x.lookup(2);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 200);
+        let ret = x.lookup(3);
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), 300);
 
-            let ret = x.delete(1);
-            assert!(ret.is_ok());
-            let ret = x.lookup(1);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(2);
-            assert!(ret.is_ok());
-            let ret = x.lookup(2);
-            assert_eq!(ret.is_ok(), false);
-            let ret = x.delete(3);
-            assert!(ret.is_ok());
-            let ret = x.lookup(3);
-            assert_eq!(ret.is_ok(), false);
-        }
+        let ret = x.delete(1);
+        assert!(ret.is_ok());
+        let ret = x.lookup(1);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(2);
+        assert!(ret.is_ok());
+        let ret = x.lookup(2);
+        assert_eq!(ret.is_ok(), false);
+        let ret = x.delete(3);
+        assert!(ret.is_ok());
+        let ret = x.lookup(3);
+        assert_eq!(ret.is_ok(), false);
+    }
 }
